b3481baea56dbfc6862ed957ecd9ec19dbebe9d0
Keith Tan <keith_xw_tan@tech.gov.sg>
2024-03-13 01:47:40 +0800

    [CCUBE-1060][KT] Set up context for elements display in element list panel

--- src/context-providers/builder/hook.ts
+++ src/context-providers/builder/hook.ts
@@ -1,20 +1,29 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { BuilderContext } from "./provider";
 
 export const useBuilder = () => {
     const { state, dispatch } = useContext(BuilderContext);
 
-    const togglePanel = (isCollapsed: boolean) => {
+    const togglePanel = useCallback((isCollapsed: boolean) => {
         dispatch({
             type: "toggle-panel",
             payload: isCollapsed,
         });
-    };
+    }, []);
+
+    const addElement = useCallback((elementId: string) => {
+        const newElementIds = [...state.elementIds, elementId];
+        dispatch({
+            type: "update-element-ids",
+            payload: newElementIds,
+        });
+    }, []);
 
     return {
         state,
         showSidePanel: state.showSidePanel,
         currentMode: state.mode,
         togglePanel,
+        addElement,
     };
 };
