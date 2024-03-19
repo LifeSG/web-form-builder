56339125b8d1851214760981cc3f5aa089749433
Keith Tan <keith_xw_tan@tech.gov.sg>
2024-03-20 00:15:55 +0800

    [CCUBE-1042][KT] Add icons for the element selector for text field category

--- src/components/common/base-card/card-icon.tsx
+++ src/components/common/base-card/card-icon.tsx
@@ -2,6 +2,7 @@ import { EnvelopeIcon } from "@lifesg/react-icons/envelope";
 import { PhoneIcon } from "@lifesg/react-icons/phone";
 import { EElementType } from "src/schemas";
 import { Container } from "./card-icon.styles";
+import { LongTextIcon, NumbersIcon, ShortTextIcon } from "../icons";
 
 interface IProps {
     elementType: EElementType;
@@ -16,6 +17,15 @@ export const CardIcon = ({ elementType }: IProps) => {
             break;
         case EElementType.CONTACT:
             icon = <PhoneIcon />;
+            break;
+        case EElementType.NUMERIC:
+            icon = <NumbersIcon />;
+            break;
+        case EElementType.TEXT:
+            icon = <ShortTextIcon />;
+            break;
+        case EElementType.TEXTAREA:
+            icon = <LongTextIcon />;
             break;
         default:
             break;
---
+++ src/components/common/icons/index.ts
@@ -0,0 +1,4 @@
+export * from "./add-element-icon";
+export * from "./long-text-icon";
+export * from "./numbers-icon";
+export * from "./short-text-icon";
