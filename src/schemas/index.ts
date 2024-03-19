7f6afb9d644ba935355a3ff5611398483789ef4f
Keith Tan <keith_xw_tan@tech.gov.sg>
2024-03-20 00:15:30 +0800

    [CCUBE-1042][KT] Switch types to interfaces and reorder EElementType enum

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,15 +2,17 @@
  * Refer to the uiType in the Frontend Engine when setting
  * the value.
  * https://designsystem.life.gov.sg/web-frontend-engine/index.html?path=/docs/introduction-getting-started--docs
+ *
+ * Alphabetical order for easier checking
  */
 export enum EElementType {
+    CHECKBOX = "checkbox",
+    CONTACT = "contact-field",
     EMAIL = "email-field",
     NUMERIC = "numeric-field",
+    RADIO = "radio",
     TEXT = "text-field",
     TEXTAREA = "textarea",
-    CONTACT = "contact-field",
-    CHECKBOX = "checkbox",
-    RADIO = "radio",
 }
 
 export interface IBaseAttributes {
@@ -23,6 +25,13 @@ export interface IBaseFieldAttributes extends IBaseAttributes {
     label: string;
 }
 
+export interface IBaseTextBasedFieldAttributes extends IBaseFieldAttributes {
+    placeholder?: string;
+}
+
+// =============================================================================
+// ELEMENT TYPES
+// =============================================================================
 export type IEmailField = IBaseFieldAttributes;
 export type INumericField = IBaseFieldAttributes;
 
