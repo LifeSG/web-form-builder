29b332d10366116c19d5978106db4f4a2eee2f05
Keith Tan <keith_xw_tan@tech.gov.sg>
2024-03-20 00:16:24 +0800

    [CCUBE-1042][KT] Switch icon to the correct one

--- src/components/side-panel/toolbar.tsx
+++ src/components/side-panel/toolbar.tsx
@@ -1,6 +1,6 @@
 import { DocIcon } from "@lifesg/react-icons/doc";
-import { Square2x2Icon } from "@lifesg/react-icons/square-2x2";
 import { EBuilderMode, useBuilder } from "../../context-providers";
+import { AddElementIcon } from "../common/icons";
 import { ModeButton, Wrapper } from "./toolbar.styles";
 
 export const Toolbar = () => {
@@ -30,7 +30,7 @@ export const Toolbar = () => {
                 $active={currentMode === EBuilderMode.ADD_ELEMENT}
                 onClick={toggleView(EBuilderMode.ADD_ELEMENT)}
             >
-                <Square2x2Icon />
+                <AddElementIcon />
             </ModeButton>
             <ModeButton
                 $active={currentMode === EBuilderMode.EDIT_PAGES}
