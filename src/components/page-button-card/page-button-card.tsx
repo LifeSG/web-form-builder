4e883f04984245976855b0b30177c4ce290be094
favian <2104315C@student.tp.edu.sg>
2024-03-25 15:02:22 +0800

    [CCUBE-1063][FT] implement basic page button card

---
+++ src/components/page-button-card/page-button-card.tsx
@@ -0,0 +1,33 @@
+import { DragHandleIcon, EllipsisHorizontalIcon } from "@lifesg/react-icons";
+import { BaseCard } from "../common";
+import { Container, PageTitle } from "./page-button-card.styles";
+import { IPage } from "src/schemas";
+import { useBuilder } from "src/context-providers";
+
+interface IProps {
+    page: IPage;
+}
+
+export const PageButtonCard = ({ page }: IProps) => {
+    const { currentPage } = useBuilder();
+
+    const renderDragger = () => {
+        // TODO: has dnd logic
+        return <DragHandleIcon />;
+    };
+
+    const renderOptions = () => {
+        // TODO: renders popover
+        return <EllipsisHorizontalIcon />;
+    };
+
+    return (
+        <BaseCard focused={currentPage.id === page.id}>
+            <Container>
+                {/* {renderDragger()} */}
+                <PageTitle>Hello World</PageTitle>
+                {/* {renderOptions()} */}
+            </Container>
+        </BaseCard>
+    );
+};
