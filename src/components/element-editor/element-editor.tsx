b69bd2afe1258a046abc717cf0a0a2fcc9a62112
Keith Tan <keith_xw_tan@tech.gov.sg>
2024-04-02 15:11:06 +0800

    [CCUBE-1058][KT] Set to use context value instead of hardcoding

--- src/components/element-editor/element-editor.tsx
+++ src/components/element-editor/element-editor.tsx
@@ -1,14 +1,20 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import { FormProvider, useForm } from "react-hook-form";
-import { EElementType } from "src/context-providers";
+import { useBuilder } from "src/context-providers";
 import { SchemaHelper } from "src/schemas";
 import { BasicDetails } from "./basic-details";
 
 export const ElementEditor = () => {
+    // =========================================================================
+    // CONST, STATE, REFS
+    // =========================================================================
+    const { focusedElement } = useBuilder();
+
     const methods = useForm({
         mode: "onTouched",
-        // TODO: insert proper type; email is a placeholder
-        resolver: yupResolver(SchemaHelper.buildSchema(EElementType.EMAIL)),
+        resolver: yupResolver(
+            SchemaHelper.buildSchema(focusedElement.element.type)
+        ),
     });
 
     return (
