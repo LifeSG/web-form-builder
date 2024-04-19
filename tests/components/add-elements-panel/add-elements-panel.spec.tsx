cd49119a44cb58086c4a2e823fffcfa31a1a7932
Ruth Shirin <shirinr.21@ichat.sp.edu.sg>
2024-04-19 09:34:09 +0800

    [CCUBE-1220][SR] Created a test for testing the clicking on the element selector card in the add element panel

--- tests/components/add-elements-panel/add-elements-panel.spec.tsx
+++ tests/components/add-elements-panel/add-elements-panel.spec.tsx
@@ -1,11 +1,26 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "jest-canvas-mock";
 import { AddElementsPanel } from "src/components/side-panel/add-elements-panel";
+import { EElementType } from "src/context-providers";
 import {
     ELEMENTS_CATEGORIES,
     ELEMENT_BUTTON_LABELS,
 } from "src/data/elements-data"; // Ensure this includes categoryTitle and elementTypes
 import { TestHelper } from "src/util/test-helper";
+
+// =============================================================================
+// MOCKS
+// =============================================================================
+const mockAddElement = jest.fn();
+jest.mock("src/context-providers/builder/hook.ts", () => {
+    const actual = jest.requireActual("src/context-providers/builder/hook.ts");
+    return {
+        useBuilder: () => ({
+            ...actual.useBuilder(),
+            addElement: mockAddElement,
+        }),
+    };
+});
 
 describe("AddElementsPanel", () => {
     afterEach(() => {
@@ -13,7 +28,18 @@ describe("AddElementsPanel", () => {
         jest.resetAllMocks();
     });
 
-    describe("renderCategories", () => {
+    describe("addElement & renderCategories", () => {
+        it("should be able to add an element when clicking on a selector card", () => {
+            renderComponent();
+            const emailElementButton = screen.getByText(
+                ELEMENT_BUTTON_LABELS[EElementType.EMAIL]
+            );
+            fireEvent.click(emailElementButton);
+            expect(mockAddElement).toHaveBeenCalledWith(
+                EElementType.EMAIL,
+                true
+            );
+        });
         it("should render all the element selector cards based on its categories", () => {
             renderComponent();
             ELEMENTS_CATEGORIES.forEach((category) => {
