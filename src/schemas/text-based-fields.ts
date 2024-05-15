de6a574de56966ff91d03b90d842a69d5628e3b0
Ruth Shirin <shirinr.21@ichat.sp.edu.sg>
2024-05-15 16:40:45 +0800

    [CCUBE-1281][SR] Amended the naming convention for the regex variables.

--- src/schemas/text-based-fields.ts
+++ src/schemas/text-based-fields.ts
@@ -5,9 +5,10 @@ import {
 import { ELEMENT_VALIDATION_TYPES } from "src/data";
 import * as yup from "yup";
 
-const DOMAIN_REGEX = /^@[^\s]+(\.[^\s]+)*(?:\s*,\s*@[^,\s]+(\.[^,\s]+)*)*$/;
-const ACTIONID_REGEX = /^[a-zA-Z0-9_-]+$/;
-const PATH_REGEX = /^[a-zA-Z0-9._-]+$/;
+const VALIDATION_DOMAIN_REGEX =
+    /^@[^\s]+(\.[^\s]+)*(?:\s*,\s*@[^,\s]+(\.[^,\s]+)*)*$/;
+const PREFILL_ACTIONID_REGEX = /^[a-zA-Z0-9_-]+$/;
+const PREFILL_PATH_REGEX = /^[a-zA-Z0-9._-]+$/;
 
 export const EMAIL_SCHEMA = yup.object<IBaseTextBasedFieldAttributes>().shape({
     placeholder: yup.string().optional(),
@@ -22,7 +23,7 @@ export const EMAIL_SCHEMA = yup.object<IBaseTextBasedFieldAttributes>().shape({
                     rule
                         .required("Email domain required.")
                         .matches(
-                            DOMAIN_REGEX,
+                            VALIDATION_DOMAIN_REGEX,
                             "Invalid email domain. Check if email domain is correct with no whitespace between characters. Separate each with a comma if there is more than 1 email."
                         ),
                 otherwise: (rule) => rule.required("Validation rule required."),
@@ -41,13 +42,13 @@ export const EMAIL_SCHEMA = yup.object<IBaseTextBasedFieldAttributes>().shape({
                 then: (rule) =>
                     rule
                         .required("Action ID required.")
-                        .matches(ACTIONID_REGEX, "Invalid action ID."),
+                        .matches(PREFILL_ACTIONID_REGEX, "Invalid action ID."),
                 otherwise: (rule) => rule.optional(),
             }),
             path: yup
                 .string()
                 .required("Path required.")
-                .matches(PATH_REGEX, "Invalid path."),
+                .matches(PREFILL_PATH_REGEX, "Invalid path."),
         })
     ),
 });
