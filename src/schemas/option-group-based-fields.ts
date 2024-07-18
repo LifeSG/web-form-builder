da13a3bf68c9b66973422384c99ffe32727adce9
Aden Teo <88359484+adenteo@users.noreply.github.com>
2024-07-18 14:24:04 +0800

    [CCUBE-1470][JY] Add custom dropdown items validation in schema

--- src/schemas/option-group-based-fields.ts
+++ src/schemas/option-group-based-fields.ts
@@ -9,11 +9,54 @@ export const OPTION_GROUP_BASED_SCHEMA = yup.object().shape({
         .array()
         .of(
             yup.object().shape({
-                label: yup.string().required("Option label required."),
-                value: yup.string().required("Option value required."),
+                label: yup.string().optional(),
+                value: yup.string().optional(),
             })
         )
-        .min(2, "At least 2 items required."),
+        .test(
+            "min-2-valid-items",
+            "At least 2 items with valid label and value are required.",
+            function (dropdownItems) {
+                const validItemsCount = dropdownItems.filter(
+                    (item) => item.label && item.value
+                ).length;
+
+                const allItemsValidOrEmpty = dropdownItems.every(
+                    (item) =>
+                        (item.label && item.value) ||
+                        (!item.label && !item.value)
+                );
+
+                if (validItemsCount >= 2 && allItemsValidOrEmpty) {
+                    return true;
+                }
+
+                const errors = dropdownItems.reduce((acc, item, index) => {
+                    if (!item.label && !item.value && validItemsCount >= 2) {
+                        return acc;
+                    }
+                    if (!item.label) {
+                        acc.push(
+                            this.createError({
+                                path: `${this.path}[${index}].label`,
+                                message: "Option label required.",
+                            })
+                        );
+                    }
+                    if (!item.value) {
+                        acc.push(
+                            this.createError({
+                                path: `${this.path}[${index}].value`,
+                                message: "Option value required.",
+                            })
+                        );
+                    }
+                    return acc;
+                }, []);
+
+                throw new yup.ValidationError(errors);
+            }
+        ),
     preselectedValue: yup.string().nullable(),
     prefill: yup.array().of(
         yup.object().shape({
