73c738bd3e92269ea143046729f3ff5fbe48c7fe
Aden Teo <88359484+adenteo@users.noreply.github.com>
2024-07-19 14:15:54 +0800

    [CCUBE-1490][JY] Remove controller wrapping divs, Add styling to remove bottom margins

--- src/components/element-editor/validation/validation-child.tsx
+++ src/components/element-editor/validation/validation-child.tsx
@@ -28,75 +28,71 @@ export const ValidationChild = ({ onDelete, options, index }: IProps) => {
     return (
         <ChildEntry onDelete={onDelete}>
             <FieldWrapper>
-                <div>
-                    <Controller
-                        name={`validation.${index}.validationType`}
-                        control={control}
-                        render={({ field }) => {
-                            const { ref, ...fieldWithoutRef } = field;
-                            return (
-                                <Form.Select
-                                    {...fieldWithoutRef}
-                                    placeholder="Select"
-                                    selectedOption={fieldWithoutRef.value}
-                                    options={options}
-                                    disabled={
-                                        options.length === 1 &&
-                                        focusedElement.element.type ===
-                                            EElementType.EMAIL
-                                    }
-                                    onSelectOption={(option) => {
-                                        field.onChange(option);
-                                    }}
-                                    errorMessage={
-                                        errors?.validation?.[index]
-                                            ?.validationType?.message
-                                    }
-                                />
-                            );
-                        }}
-                    />
-                </div>
-                <div>
-                    <Controller
-                        name={`validation.${index}.validationRule`}
-                        control={control}
-                        render={({ field }) => {
-                            const { ref, ...fieldWithoutRef } = field;
-                            return (
-                                <ValidationRule
-                                    fieldWithoutRef={{ ...fieldWithoutRef }}
-                                    index={index}
-                                    validationType={validationType}
-                                    errors={errors}
-                                />
-                            );
-                        }}
-                    />
-                </div>
-                <div>
-                    <Controller
-                        name={`validation.${index}.validationErrorMessage`}
-                        control={control}
-                        render={({ field }) => {
-                            const { ref, ...fieldWithoutRef } = field;
-                            return (
-                                <Form.Input
-                                    {...fieldWithoutRef}
-                                    placeholder="Set error message"
-                                    value={fieldWithoutRef.value}
-                                    onChange={(event) => {
-                                        field.onChange(event.target.value);
-                                    }}
-                                    errorMessage={
-                                        errors?.validation?.[index]
-                                            ?.validationErrorMessage?.message
-                                    }
-                                />
-                            );
-                        }}
-                    />
-                </div>
+                <Controller
+                    name={`validation.${index}.validationType`}
+                    control={control}
+                    render={({ field }) => {
+                        const { ref, ...fieldWithoutRef } = field;
+                        return (
+                            <Form.Select
+                                {...fieldWithoutRef}
+                                placeholder="Select"
+                                selectedOption={fieldWithoutRef.value}
+                                options={options}
+                                disabled={
+                                    options.length === 1 &&
+                                    focusedElement.element.type ===
+                                        EElementType.EMAIL
+                                }
+                                onSelectOption={(option) => {
+                                    field.onChange(option);
+                                }}
+                                errorMessage={
+                                    errors?.validation?.[index]?.validationType
+                                        ?.message
+                                }
+                            />
+                        );
+                    }}
+                />
+
+                <Controller
+                    name={`validation.${index}.validationRule`}
+                    control={control}
+                    render={({ field }) => {
+                        const { ref, ...fieldWithoutRef } = field;
+                        return (
+                            <ValidationRule
+                                fieldWithoutRef={{ ...fieldWithoutRef }}
+                                index={index}
+                                validationType={validationType}
+                                errors={errors}
+                            />
+                        );
+                    }}
+                />
+
+                <Controller
+                    name={`validation.${index}.validationErrorMessage`}
+                    control={control}
+                    render={({ field }) => {
+                        const { ref, ...fieldWithoutRef } = field;
+                        return (
+                            <Form.Input
+                                {...fieldWithoutRef}
+                                placeholder="Set error message"
+                                value={fieldWithoutRef.value}
+                                onChange={(event) => {
+                                    field.onChange(event.target.value);
+                                }}
+                                errorMessage={
+                                    errors?.validation?.[index]
+                                        ?.validationErrorMessage?.message
+                                }
+                            />
+                        );
+                    }}
+                />
             </FieldWrapper>
         </ChildEntry>
     );
