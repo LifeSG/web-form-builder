56141852006aae8b675ef8375312190cf5fc94a5
Aden Teo <88359484+adenteo@users.noreply.github.com>
2024-09-06 09:53:00 +0800

    [CCUBE-1541][JY] Remove config from shared doc-element

--- stories/doc-elements.tsx
+++ stories/doc-elements.tsx
@@ -8,36 +8,7 @@ interface IProps {
 export const DocElement = ({ onSubmit }: IProps) => {
     return (
         <div style={{ height: "100vh", width: "100vw" }}>
-            <FormBuilder
-                onSubmit={onSubmit}
-                config={{
-                    elements: {
-                        Dropdown: {
-                            shouldShow: true, // Dropdown element would be hidden from the Add Elements panel
-                            attributes: {
-                                id: {
-                                    shouldShow: true,
-                                },
-                            },
-                        },
-                        "Contact number": {
-                            attributes: {
-                                type: {
-                                    shouldShow: false,
-                                },
-                            },
-                        },
-                    },
-                    attributes: {
-                        id: {
-                            shouldShow: false,
-                        },
-                        type: {
-                            shouldShow: false,
-                        },
-                    },
-                }}
-            />
+            <FormBuilder onSubmit={onSubmit} />
         </div>
     );
 };
