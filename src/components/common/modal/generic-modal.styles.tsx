fb080a06c93abe176981823a51a94e24fb725ce1
Wei Li <ng_wei_li@tech.gov.sg>
2025-04-20 17:42:32 +0800

    [CCUBE-1703][WL] remove unused media queries

--- src/components/common/modal/generic-modal.styles.tsx
+++ src/components/common/modal/generic-modal.styles.tsx
@@ -1,4 +1,3 @@
-import { MediaQuery } from "@lifesg/react-design-system/theme";
 import { Modal } from "@lifesg/react-design-system/modal";
 import styled from "styled-components";
 
@@ -16,18 +15,10 @@ export const ModalBox = styled(Modal.Box)`
 export const ModalInner = styled.div`
     margin: 4rem;
     text-align: center;
-
-    ${MediaQuery.MaxWidth.sm} {
-        margin: 4rem 1.25rem;
-    }
 `;
 
 export const GrowContainer = styled.div`
     margin: auto;
     padding: 5rem 1.25rem;
     width: 100%;
-
-    ${MediaQuery.MaxWidth.sm} {
-        padding: 1rem 1.25rem;
-    }
 `;
